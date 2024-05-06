cdfaf7d0d0b66ed610b41e528702e6289d1ec4b9
Diego Garcia <digery@gmail.com>
2024-05-05 22:06:03 -0500

    My first commit

---
+++ src/Main.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.println("Bienvenido a la imersion en Java!");
+        //Declaración de variables
+        //System.out.println("Pelicula Matrix");
+
+        int fechaDeLanzamiento = 1999;
+        double evaluacion = 4.5;
+        boolean incluidoEnElPlanBasico = true;
+        String nombre = "Matrix";
+        String sinopsis = """
+                La mejor pelicula del fin del milenio
+                """;
+
+        double mediaEvaluacionUsuario = 0;
+
+        System.out.println("Pelicula: " + nombre);
+        System.out.println(fechaDeLanzamiento);
+        System.out.println(evaluacion);
+        System.out.println(incluidoEnElPlanBasico);
+
+        double mediaEvaluacion = (4.5 + 4.8 + 3) / 3;
+        System.out.println("Media de la evaluacion de Matriz: " + mediaEvaluacion);
+
+
+        if (fechaDeLanzamiento >= 2023){
+            System.out.println("Pelicula Popular en el momento");
+        } else {
+            System.out.println("Pelicula retro que vale la pena ver");
+        }
+
+        for (int i = 0; i < 3; i++) {
+            Scanner teclado = new Scanner(System.in);
+            System.out.println("ingresa la nota que le darias a Matrix");
+            double notaMatrix = teclado.nextDouble();
+            mediaEvaluacionUsuario = mediaEvaluacionUsuario + notaMatrix;
+        }
+        System.out.println("La media de la pelicula " +
+                "Matrix calculada por el usuario es:" + mediaEvaluacionUsuario / 3);
+    }
+}
